1bb1c2cdbf6405a00a3224605f81b047885e68bf
Dan Michael O. Heggø <danmichaelo@gmail.com>
2019-01-21 14:12:44 +0100

    Queue connections and limit max number of open connections

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,31 @@
 import http from './http';
 const axiosCookieJarSupport = require('axios-cookiejar-support').default;
 
-const apiCall = (args) => {
-    return http.request(args);
+let queue = [];
+let openConnections = 0;
+let maxOpenConnections = 10;
+
+function apiCall (args) {
+    return new Promise((resolve, reject) => {
+        queue.push({args: args, resolve: resolve, reject: reject})
+        processQueue()
+    })
+}
+
+function processQueue () {
+    if (!queue.length || openConnections >= maxOpenConnections) {
+        return;
+    }
+
+    openConnections++;
+    let request = queue.shift();
+    http.request(request.args)
+    .then(res => request.resolve(res))
+    .catch(res => request.reject(res))
+    .finally(() => {
+        openConnections--
+        processQueue()
+    })
 }
 
 export default apiCall
